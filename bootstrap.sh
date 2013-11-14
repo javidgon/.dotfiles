258578740b2d02c7cdb75316760c6a323ed534c0
Jose Vidal <jose.vidal@softwarediagnostics.com>
2013-11-14 14:40:15 +0100

    Improve structure in order to make it more understandable

--- bootstrap.sh
+++ bootstrap.sh
@@ -28,30 +28,39 @@ echo "3) Configuring .dotfiles and settings."
 if [ -d .vim ]
 then
     echo "* VIM: Install Vim folder (plugins, scripts...) in ~/ ..."
+    if [ -d ~/.vim ]
+    then
+        echo "* VIM: '~/.vim' folder already exists. I'll replace it, sorry..."
+        rm -rf ~/.vim
+    fi
     # Copy vim folder into the user directory.
+    echo "* VIM: Create symbolic link of '~/.vim' folder..."
     sudo ln -s -f `pwd`/.vim/ ~/
-    echo "* VIM: Create symbolic link of .vimrc"
+    echo "* VIM: Create symbolic link of '~/.vimrc' file..."
     # Create symbolic link of .vimrc.
     sudo ln -s -f `pwd`/.vim/.vimrc ~/.vimrc
 fi
 
+if [ -d .tmux/ ]
+then
+    echo "* TMUX: Create symbolic link of '~/.tmux.conf' file..."
+    sudo ln -s -f `pwd`/.tmux/.tmux.conf ~/.tmux.conf
+fi
+
 if [ -d .tmuxinator/ ]
 then
-    echo "* TMUXINATOR: Setting up EDITOR environment var to VIM"
+    echo "* TMUXINATOR: Setting up EDITOR environment var to VIM..."
     export EDITOR="vim"
 
-    echo "* TMUXINATOR: Configuring predefined sessions"
+    echo "* TMUXINATOR: Configuring predefined sessions..."
     if [ -d ~/.tmuxinator/ ]
     then
-        echo "* TMUXINATOR: '~/.tmuxinator' folder already exist, I don't need to create it ..."
+        echo "* TMUXINATOR: '~/.tmuxinator' folder already exist, I don't need to create it then..."
     else
-        echo "* TMUXINATOR: '~/.tmuxinator' folder does not exist, I will have to create it ..."
+        echo "* TMUXINATOR: '~/.tmuxinator' folder does not exist, I will have to create it then..."
         mkdir ~/.tmuxinator
     fi
     # Sessions to install.
-    echo "* TMUXINATOR: Deploy different sessions."
-    sudo ln -s -f `pwd`/.tmuxinator/sessions/dev.yml ~/.tmuxinator/
-    # Deploy .tmux.conf
-    echo "* TMUXINATOR: Create symbolic link of .tmux.conf"
-    sudo ln -s -f `pwd`/.tmuxinator/.tmux.conf ~/.tmux.conf
+    echo "* TMUXINATOR: Create symbolic link of '~/.tmuxinator/dev.yml' file..."
+    sudo ln -s -f `pwd`/.tmuxinator/dev.yml ~/.tmuxinator/
 fi
